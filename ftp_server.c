0d5280265ec5676482a61eb8498477a017b89324
hendrmat <hendrmat@mail.gvsu.edu>
2020-02-15 15:15:45 -0500

    Fixed typos

--- ftp_server.c
+++ ftp_server.c
@@ -6,10 +6,16 @@
 */
 
 // Adding list functionality soon, establishing hendrick branch
+
 #include <stdio.h>
+#include <stdlib.h>
 #include <sys/types.h> 
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <netdb.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
 
 void dostuff(int); /* function prototype */
 void error(char *msg)
@@ -20,40 +26,44 @@ void error(char *msg)
 
 int main(int argc, char *argv[])
 {
-     int sockfd, newsockfd, portno, clilen, pid;
+     int sockfd, newsockfd, portno, clilen, pid, status, run;
      struct sockaddr_in serv_addr, cli_addr;
 
-     if (argc < 2) {
-         fprintf(stderr,"ERROR, no port provided\n");
-         exit(1);
-     }
      sockfd = socket(AF_INET, SOCK_STREAM, 0);
-     if (sockfd < 0) 
+     if (sockfd < 0){ 
         error("ERROR opening socket");
+     }
      bzero((char *) &serv_addr, sizeof(serv_addr));
-     portno = atoi(argv[1]);
      serv_addr.sin_family = AF_INET;
      serv_addr.sin_addr.s_addr = INADDR_ANY;
      serv_addr.sin_port = htons(portno);
      if (bind(sockfd, (struct sockaddr *) &serv_addr,
-              sizeof(serv_addr)) < 0) 
+              sizeof(serv_addr)) < 0) {
               error("ERROR on binding");
+     }
      listen(sockfd,5);
      clilen = sizeof(cli_addr);
-     while (1) {
+     run = 1;
+     while (run) {
          newsockfd = accept(sockfd, 
                (struct sockaddr *) &cli_addr, &clilen);
-         if (newsockfd < 0) 
+         if (newsockfd < 0) { 
              error("ERROR on accept");
+         }
          pid = fork();
-         if (pid < 0)
+         if (pid < 0){
              error("ERROR on fork");
+         }
          if (pid == 0)  {
              close(sockfd);
              dostuff(newsockfd);
              exit(0);
          }
-         else close(newsockfd);
+         else {
+             wait (&status);
+             close(newsockfd);
+             run = 0;
+         }
      } /* end of while */
      return 0; /* we never get here */
 }

64ae8f14dc43b9e1c44717b30ccb05c03e018848
Reuben Nyenhuis <nyenhuir@eos02.cis.gvsu.edu>
2020-02-18 20:36:19 -0500

    Added 'list' function -Reuben

--- ftp_server.c
+++ ftp_server.c
@@ -4,20 +4,19 @@
    process for each connection
    gcc server2.c -lsocket
 */
-
-// Adding list functionality soon, establishing hendrick branch
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h> 
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <netdb.h>
 #include <string.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <string.h>
+#include <dirent.h>
 
 void dostuff(int); /* function prototype */
+void list(int);
 void error(char *msg)
 {
     perror(msg);
@@ -26,44 +25,45 @@ void error(char *msg)
 
 int main(int argc, char *argv[])
 {
-     int sockfd, newsockfd, portno, clilen, pid, status, run;
+     int sockfd, newsockfd, portno, clilen, pid,run,status;
      struct sockaddr_in serv_addr, cli_addr;
-  
+
+     //if (argc < 2) {
+     //  fprintf(stderr,"ERROR, no port provided\n");
+     //  exit(1);
+     //}
      sockfd = socket(AF_INET, SOCK_STREAM, 0);
-     if (sockfd < 0){ 
+     if (sockfd < 0) 
         error("ERROR opening socket");
-     }
      bzero((char *) &serv_addr, sizeof(serv_addr));
+     //portno = atoi(argv[1]);
      serv_addr.sin_family = AF_INET;
      serv_addr.sin_addr.s_addr = INADDR_ANY; //inet_addr("192.168.110.7");
      serv_addr.sin_port = htons(6550);
      if (bind(sockfd, (struct sockaddr *) &serv_addr,
-              sizeof(serv_addr)) < 0) {
+              sizeof(serv_addr)) < 0) 
               error("ERROR on binding");
-     }
      listen(sockfd,5);
      clilen = sizeof(cli_addr);
-     run = 1;
+     run=1;
      while (run) {
          newsockfd = accept(sockfd, 
                (struct sockaddr *) &cli_addr, &clilen);
-         if (newsockfd < 0) { 
+         if (newsockfd < 0) 
              error("ERROR on accept");
-         }
          pid = fork();
-         if (pid < 0){
+         if (pid < 0)
              error("ERROR on fork");
-         }
          if (pid == 0)  {
              close(sockfd);
              dostuff(newsockfd);
              exit(0);
          }
-         else {
-             wait (&status);
-             close(newsockfd);
-             run = 0;
-         }
+         else{
+		wait (&status);
+		//close(newsockfd);
+	 	//run=0;
+	 }
      } /* end of while */
      return 0; /* we never get here */
 }
@@ -81,7 +81,29 @@ void dostuff (int sock)
    bzero(buffer,256);
    n = read(sock,buffer,255);
    if (n < 0) error("ERROR reading from socket");
+
+   printf("RECEIVED: %s.",buffer);
+   if(strcmp(buffer,"LIST")==0)
+	list(sock);
    printf("Here is the message: %s\n",buffer);
    n = write(sock,"I got your message",18);
+   if (n < 0) error("ERROR writing to socket");
+}
+
+void list(int sock){
+   char files[1000];
+   int n;
+   DIR *d;
+   struct dirent *dir;
+   d = opendir(".");
+   if(d){
+	while((dir = readdir(d)) != NULL){
+		strcat(files,dir->d_name);
+		strcat(files,"\n");
+	}
+	closedir(d);
+   }
+   
+   n = write(sock,files,strlen(files));
    if (n < 0) error("ERROR writing to socket");
 }
